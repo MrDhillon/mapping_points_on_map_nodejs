f22d9fa18fde193046c219557ba18433abd3a023
MrDhillon <harman.singh.dhillon@gmail.com>
2015-11-21 05:16:44 -0500

    adding visitors to pop up

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,6 +4,7 @@ function markerPost(){
   $.post('/',{marker: $id }, function(res){
     $('.modal-content').html("<p> Store name: "+res[0].name+"</p>\
                               <p> Located in: "+res[0].city+","+res[0].country+"</p>\
+                              <p> Visitors: "+res[0].total_visitors+"</p>\
                               <p> Start of Day: "+res[0].start_of_day+"</p>")
    $('#myModal').modal('toggle');
   });
